e505d9651f9daf4f39b44c772d7485bea037bfe8
GNS <gnsgns199@gmail.com>
2023-02-23 15:41:25 +0530

    Basic Pys

--- PyBasics.py
+++ PyBasics.py
@@ -5,5 +5,34 @@ num= int(input("Enter a Number"));
 sqrt= num**(1/2);
 print("Square Root is", sqrt)
 
+# Python Program to Calculate the Area of a Triangle
+a=int(input("Enter length "))
 
+
+# Python Program to Solve Quadratic Equation
+# Python Program to Swap Two Variables
+# Python Program to Generate a Random Number
+# Python Program to Convert Kilometers to Miles
+# Python Program to Convert Celsius To Fahrenheit
+# Python Program to Check if a Number is Positive, Negative or 0
+# Python Program to Check if a Number is Odd or Even
+# Python Program to Check Leap Year
+# Python Program to Find the Largest Among Three Numbers
+# Python Program to Check Prime Number
+# Python Program to Print all Prime Numbers in an Interval
+# Python Program to Find the Factorial of a Number
+# Python Program to Display the multiplication Table
+# Python Program to Print the Fibonacci sequence
+# Python Program to Check Armstrong Number
+# Python Program to Find Armstrong Number in an Interval
+# Python Program to Find the Sum of Natural Numbers
+# Python Program to Display Powers of 2 Using Anonymous Function
+# Python Program to Find Numbers Divisible by Another Number
+# Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+# Python Program to Find ASCII Value of Character
+# Python Program to Find HCF or GCD
+# Python Program to Find LCM
+# Python Program to Find the Factors of a Number
+# Python Program to Make a Simple Calculator
+# Python Program to Shuffle Deck of Cards
 
