ab727c0ff78aef14c61cc4afc41d99856ae6c7dc
GNS <gnsgns199@gmail.com>
2023-02-23 15:30:33 +0530

    Basic Pys

--- PyBasics.py
+++ PyBasics.py
@@ -1,4 +1,9 @@
 # .................................. Python Basics Programs....................................
 
 # Find Square root of a number
+num= int(input("Enter a Number"));
+sqrt= num**(1/2);
+print("Square Root is", sqrt)
+
+
 
