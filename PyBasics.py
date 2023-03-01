78c8f236b854a3a761858cf088bbfd924ac5240a
gns199 <69713591+gns199@users.noreply.github.com>
2023-03-01 22:05:15 +0530

    Update PyBasics.py

--- PyBasics.py
+++ PyBasics.py
@@ -1,11 +1,11 @@
 # .................................. Python Basics Programs....................................
 
-# Find Square root of a number
+# 1. Find Square root of a number
 num= int(input("Enter a Number"))
 sqrt= num**(1/2)
 print("Square Root is", sqrt)
 
-# Python Program to Calculate the Area of a Triangle
+# 2. Python Program to Calculate the Area of a Triangle
 a=int(input("Enter 1st length: "))
 b=int(input("Enter 2nd length: "))
 c=int(input("Enter 3rd length: "))
@@ -13,14 +13,14 @@ area= (a+b+c)/2;
 print("Area is",area)
 
 
-# Python Program to Solve Quadratic Equation -b+ root(b**2-4ac)/2a
+# 3. Python Program to Solve Quadratic Equation -b+ root(b**2-4ac)/2a
 b=int(input("Enter a number"))
 a=int(input("Enter 2nd no"))
 c=int(input("Enter 3rd no"))
 d=(-b+((b**2-(4*a*c))**0.5))/(2*a)
 print(d)
 
-# Python Program to Swap Two Variables
+# 4. Python Program to Swap Two Variables
 a = int(input("Enter a number"))
 b=int(input("Enter 2nd number"))
 c=a
@@ -29,17 +29,17 @@ b=c
 print(a)
 print(b)
 
-# Python Program to Convert Kilometers to Miles
+# 5. Python Program to Convert Kilometers to Miles
 km = int(input("Enter in KMs"))
 miles=km*0.621
 print(miles,"miles")
 
-# Python Program to Convert Celsius To Fahrenheit
+# 6. Python Program to Convert Celsius To Fahrenheit
 cel = int(input("Enter Temperature"))
 far=(cel*1.8)+32
 print(far,"Fahrenheit")
 
-# Python Program to Check if a Number is Positive, Negative or 0
+# 7. Python Program to Check if a Number is Positive, Negative or 0
 a=int(input("Enter a number"))
 if a>0:
     print("Positive")
@@ -47,20 +47,20 @@ elif a<0:
     print("Negative")
 else:
     print("Zero")
-# Python Program to Check if a Number is Odd or Even
+# 8. Python Program to Check if a Number is Odd or Even
 num=int(input("Enter a number: "))
 if num%2==0:
     print("It is a even number")
 else:
     print("It is odd number")
-# Python Program to Check Leap Year
+# 9. Python Program to Check Leap Year
 year=int(input("Enter Year: "))
 if year%4==0:
     print("It is leap year")
 else:
     print("It is not a leap year")
 
-# Python Program to Find the Largest Among Three Numbers
+# 10. Python Program to Find the Largest Among Three Numbers
 a=int(input("Enter 1st Number"))
 b=int(input("Enter 2nd Number"))
 c=int(input("Enter 3rd Number"))
@@ -73,7 +73,7 @@ elif c>a:
 else:
     print("Incorrect Number")
 
-# Python Program for simple interest
+# 11. Python Program for simple interest
 p=int(input("Enter Principal Amount: "))
 t=int(input("Enter time duration: "))
 r=int(input("Enter rate of interest: "))
@@ -81,57 +81,57 @@ i=(p*t*r)/100;
 print("Interest is:",i)
 print("Total is: ",p+i)
 
-#Area of Circle
+# 12. Area of Circle
 r=int(input("Enter radius"))
 area=3.14*r*r
 print(area)
 
-#Check Vowel or Consonant using if-else
+# 13. Check Vowel or Consonant using if-else
 a=input("Enter an alphabet: ")
 if a=="a" or a=="e" or a=="i" or a=="o" or a=="u":
     print("It is a vowel")
 else:
     print("It is a consonant")
 
-#Average of three numbers
+# 14. Average of three numbers
 a=int(input("Enter First Number: "))
 b=int(input("Enter Second Number: "))
 c=int(input("Enter Third Number: "))
 avg=(a+b+c)/3
 print("Average is: ",avg)
 
-#Find Area of Square
+# 15. Find Area of Square
 l=int(input("Enter Length"))
 area=l**2
 print(area)
 
-#Find Area of Rectangle
+# 16.0Find Area of Rectangle
 l=int(input("Enter Length: "))
 b=int(input("Enter breadth: "))
 print("area is: ",l*b)
 
-#Find Area of Triangle based on Base and Height
+# 17. Find Area of Triangle based on Base and Height
 b=int(input("Enter Base of Triangle: "))
 h=int(input("Enter Height of Triangle: "))
 area=(1/2)*b*h
 print(area)
 
-#circumferrence of a circle
+# 18. circumferrence of a circle
 r=int(input("Enter Radius:"))
 circ=2*3.14*r
 print("The Circumferrence is:",circ)
 
-#Python program to find Cube of a Number
+# 19. Python program to find Cube of a Number
 n=int(input("Enter a number"))
 cube=n**3
 print("Cube is: ",cube)
 
-#Python program to find square root of a Number
+# 20. Python program to find square root of a Number
 n=int(input("Enter a number"))
 sqrt=n**0.5
 print("Square root is: ",sqrt)
 
-#Python program to find Square of a Number
+# 21. Python program to find Square of a Number
 n=int(input("Enter a number"))
 sq=n**2
 print("Square is: ",sq)
